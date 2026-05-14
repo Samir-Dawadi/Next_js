dd07553cafdd15add2137e476c68309e95a75973
Samir-Dawadi <samirdawadi724@gmail.com>
2026-05-14 11:40:18 +0545

    Added metadata for layout

--- app/(CMS)/layout.tsx
+++ app/(CMS)/layout.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from "next"
 import { ReactNode } from "react"
+
+export const metadata: Metadata = {
+    title: "CMS Page Detail",
+    description: ""
+}
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (<html>
         <body>
-            
+
             {children}
         </body>
     </html>
--- app/(HOME)/layout.tsx
+++ app/(HOME)/layout.tsx
@@ -2,6 +2,12 @@ import { ReactNode } from "react";
 import "../globals.css"
 import Footer from "@/components/Footer/footer";
 import Header from "@/components/Header/header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Home Page for Eommerce",
+    description: "Share garda dekhini extra info like if we share insta photos then first image , tilte and description "
+}
 
 export default function RootLayout({children}:Readonly<{children:ReactNode}>){
   return (<>
